213bad9597a8a9c495c76a879a04c287adb62126
Bennet Yee <bennet.yee@gmail.com>
2021-03-16 14:00:09 -0700

    backup before delete old
    plus some cleanups.  allow naming dirs to backup after media path.
    allow rsync exclude arguments (any rsync argument, really) to be
    specified using an environment variable.  improve error handling.
    
    currently the default directories to back up include the old /mm/$USER
    path; at some point we may want to remove that.
    
    the semantics is to try to back up all directories even if one failed;
    the script's exit status is non-zero if any failures occurred.

--- backup.cmd
+++ backup.cmd
@@ -1,17 +1,71 @@
 #!/bin/bash
 
+# A script to do differential backups.  Backups performed at different
+# times are kept in separate directories, but if a file has not
+# changed then these directories will share the file via a hard link.
+# This saves disk space.  The assumption is that the backup is done
+# via a removable storage device, e.g., USB external storage.  There
+# is currently no provision to use ssh/rsync to back up to a remote
+# machine, though this may be added in the future.  After backing up
+# one's home directory, the assumption is that the external storage
+# device will be unmounted, removed, and stored somewhere safe,
+# possibly away from the computer to avoid common mode failures such
+# as physical damage scenarios.
+
 u=${USER:-$(whoami)}
 media=${1:-"/media/$u/backup"}
+shift
+
 dest=${media}/${u}/$(hostname)/backup
-max_backup=64
+digits=3
+max_backup=$(printf %0${digits}d ${MAXBACKUP:-64})
+
 VERBOSE=${VERBOSE:-0}
+
+EXCLUDES=${EXCLUDES:-"--exclude=/.Private \
+       --exclude=/.aptitude \
+       --exclude=/.cache \
+       --exclude=/.gvfs \
+       --exclude=/.gxine/socket \
+       --exclude=/.rnd \
+       --exclude=/mail/**/.*"}
+
+# Use a fixed number of digits as the backup directory name, so that
+# when we do ls etc the directories will lexicographically sort in a
+# nice order.
+
+# BUG: if MAXBACKUP is more than 3 digits, then we overflow the printf
+# format, and some names will use 3 digits and others will be 4.
+
+# rest of argument list is a list of source directories to back up.
+if [ "$*" = '' ]
+then
+	set /home/$u /mm/$u
+fi
+
+for d
+do
+	case "$d" in
+	/*)
+		;;
+	*)
+		printf 'Fatal: source directory for backup %s is not absolute' "$d" >&2
+		exit 1
+		;;
+	esac
+done
 
 function shift_backup() {
 	# shift_backup /foo/bar 001  -- renames /foo/bar.001 to /foo/bar.002
+	# Handles number of digits used in the numbering.
 	if [ -d "$1.$2" ]
 	then
 		[ $VERBOSE -ge 2 ] && echo "shifting $1.$2"
-		next=$(printf %03d $((10#$2 + 1)))
+		next=$(printf %0${digits}d $((10#$2 + 1)))
+		# $(( )) use of # not well documented: it says that
+		# the $2 should be evaluated as base 10, even if
+		# e.g. it may start with a 0 and otherwise would have
+		# been interpreted as octal.
 		mv -f "$1.$2" "$1.$next"
 	fi
 }
@@ -22,51 +76,75 @@ function backup() {
 	target_dir="$2"
 	if [ ! -d "$src_dir" ]
 	then
-		echo "source directory $src_dir does not exist; skipping..."
 		return 1
 	fi
 	[ -d "$target_dir" ] || mkdir -p "$target_dir"
-	shift; shift
+	shift; shift  # consume dirs, so "$@" are the rsync options
+
+	# If we run out of disk space or inodes, the cp -al or rsync
+	# below will fail.  We leave behind the new_dir, but existing
+	# data should be fine.  Manual cleanup is needed for the new
+	# directory, changing the max number of backup to save, etc.
+	new_dir="$target_dir/backup.new.$$"
+	most_recent="$target_dir/backup.$(printf %0${digits}d 0)"
+	if [ -d "$most_recent" ]
+	then
+		cp -al "$most_recent" "$new_dir"
+	else
+		[ $VERBOSE -ge 1 ] && printf 'Not previously backed up'
+	fi
+	rsync -a --delete "$@" "$src_dir/" "$new_dir/"
+
+	# Create a timestamp file, so when the backup is done is obvious.
+	touch "$new_dir/BACKUP.$(date -Isec)"
+
 	if [ -d "$target_dir/backup.$max_backup" ]
 	then
-		[ $VERBOSE -ge 1 ] && echo "at max backup limit $max_backup; removing excess"
-		( mv -f "$target_dir/backup.$max_backup" "$target_dir/trash";
-		  chmod -R u+w "$target_dir/trash";
-		  rm -fr "$target_dir/trash" & )
+		[ $VERBOSE -ge 1 ] && printf 'at max backup limit %d; removing excess' "$max_backup"
+		trash="$target_dir/trash.$$"
+		mv -f "$target_dir/backup.$max_backup" "$trash";
+		# trash removing in the background
+		(chmod -R u+w "$trash" && rm -fr "$trash")&
 	fi
-	i="$max_backup"
-	while (($i >= 0))
+	for i in $(seq $((10#$max_backup - 1)) -1 0)
 	do
-		i=$(($i - 1))
-		shift_backup "$target_dir/backup" "$(printf %03d $i)"
+		shift_backup "$target_dir/backup" "$(printf %0${digits}d $i)"
 	done
-	if [ -d "$target_dir/backup.001" ]
+
+	if [ -d "$most_recent" ]
 	then
-		cp -al "$target_dir/backup.001" "$target_dir/backup.000"
-	else
-		[ $VERBOSE -ge 1 ] && echo 'Not previously backed up'
+		echo "most-recent backup $most_recent still exists after shifting?" >&2
+		exit 1
 	fi
-	rsync -a --delete "$@" "$src_dir/" "$target_dir/backup.000/"
-	touch "$target_dir/backup.000/BACKUP.$(date -Isec)"
+
+	mv "$new_dir" "$most_recent"
+	sync
 }
 
+# In case the system is not configured to automatically mount the
+# removable device's filesystem.
 if [ ! -d "$media" ]
 then
 	echo "backup destination $media not mounted?" >&2
 	exit 1
 fi
 
-backup "/home/$u" "$dest/home/$u" \
-       --exclude=/.Private \
-       --exclude=/.aptitude \
-       --exclude=/.cache \
-       --exclude=/.gvfs \
-       --exclude=/.gxine/socket \
-       --exclude=/.rnd \
-       --exclude=/mail/**/.*
-backup "/mm/$u" "$dest/mm/$u"
-sync
-echo 'Done.'
+status=0
+
+for d
+do
+	if ! backup "$d" "$dest$d" $EXCLUDES
+	then
+		printf 'backup of %s failed\n' "$d" >&2
+		status=1
+	fi
+done
+
+[ $VERBOSE -ge 1 ] && printf 'Done.\n'
+
+# Suggest how to unmount the external drive.  Not done automatically,
+# in case the user wants to look around, or if the drive is a "hot"
+# backup also used for other things.
 
 info=$(mount | grep "$media")
 case "$info" in
@@ -82,12 +160,13 @@ case "$info" in
 	bdev=$(echo "$info" | sed 's/[0-9]*[ \t].*//')
 	;;
 *)
-	echo "No guesses on how it is mounted"
-	echo "$info"
+	printf 'No guesses on how backup media is mounted\n'
+	printf ' device: %s\n' "$info"
 	;;
 esac
 
-echo 'You should run something like:'
-echo " pumount $media"
-echo " udisksctl power-off -b ${bdev:-'block-device'}" # /dev/sdc
-echo 'before unplugging the external drive.'
+printf 'You should run something like:\n'
+printf ' pumount %s\n' "$media"
+printf ' udisksctl power-off -b %s\n' "${bdev:-'block-device'}" # /dev/sdc
+printf 'before unplugging the external drive.\n'
+exit $status
